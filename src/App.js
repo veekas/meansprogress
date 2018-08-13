d61e2c9b63c3a00bb6bae26185ba9f23fff80c1f
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-08-12 23:15:15 -0400

    🐇 🥚

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+
+import { QUOTE, VMP_ASCII } from './utils';
+
 import './App.css';
 
 export default class App extends Component {
   render() {
+    console.log(QUOTE, '\n', VMP_ASCII);
+
     return (
       <div>
       </div>
