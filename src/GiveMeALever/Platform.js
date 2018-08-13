4bae2182f4f5af3ac4d52925942df3559dc2b730
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-08-13 02:16:54 -0400

    in progress

--- src/GiveMeALever/Platform.js
+++ src/GiveMeALever/Platform.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Tappable from 'react-tappable';
+import { Motion, spring } from 'react-motion';
 
-export const Platform = () => (
-  <div>
-    <img
-      className='platform'
-      src='assets/vmp-logo-platform.png'
-      alt='platform icon'
-    />
-  </div>
-);
+export default class Platform extends Component {
+  handleTapEvent = () => {
+    console.log('hello');
+  };
 
-export default Platform;
+  render() {
+    return (
+      <Tappable onTap={this.handleTapEvent}>
+        <Motion defaultStyle={{ x: 0 }} style={{ x: spring(100) }}>
+          {style => (
+            <img
+              style={style}
+              // className="platform"
+              src="assets/vmp-logo-platform.png"
+              alt="platform icon"
+            />
+          )}
+        </Motion>
+      </Tappable>
+    );
+  }
+}
