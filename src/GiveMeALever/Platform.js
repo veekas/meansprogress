55088990711b2bd42c193d23392b913dde56e617
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-08-13 01:37:19 -0400

    add giveMeALever images and default placement

---
+++ src/GiveMeALever/Platform.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Platform = () => (
+  <div>
+    <img
+      className='platform'
+      src='assets/vmp-logo-platform.png'
+      alt='platform icon'
+    />
+  </div>
+);
+
+export default Platform;
---
+++ src/GiveMeALever/index.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+import World from './World';
+import Platform from './Platform';
+import Lever from './Lever';
+
+import './styles.css';
+
+export const GiveMeALever = () => {
+  return (
+    <Fragment>
+      <World />
+      <Platform />
+      <Lever />
+    </Fragment>
+  );
+}
+
+export default GiveMeALever;
