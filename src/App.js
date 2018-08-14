a490eb3dca67be65dfcabad1413cae71764dac82
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-08-14 13:13:27 -0400

    move position and tap logic to app state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
-import { QUOTE, VMP_ASCII } from './utils';
+// import { QUOTE, VMP_ASCII, VMP_ASCII_VERT, FLAT, LEFT_DOWN, RIGHT_DOWN } from './utils';
+import utils from './utils';
 import GiveMeALever from './GiveMeALever';
 
 import './App.css';
 
+const { QUOTE, VMP_ASCII, FLAT, LEFT_DOWN, RIGHT_DOWN } = utils;
+
 export default class App extends Component {
+  initialState = { position: FLAT }
+  state = this.initialState;
+
+  handleTapEvent = () => {
+    const position = this.state.position >= FLAT ? LEFT_DOWN : RIGHT_DOWN;
+    this.setState({ position });
+  };
+
   render() {
+    const { position } = this.state;
     console.log(QUOTE, '\n', VMP_ASCII);
+    console.log(position);
 
     return (
       <div className='give-me-a-lever-container'>
-        <GiveMeALever />
+        <GiveMeALever
+          handleTapEvent={this.handleTapEvent}
+          position={position}
+        />
       </div>
     );
   }
