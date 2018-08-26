c3fab12e08112807651b2febdcf03d4b94a3a759
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-08-26 11:30:37 -0400

    update move the world hint

--- src/Hint/index.js
+++ src/Hint/index.js
@@ -4,19 +4,23 @@ import { FLAT } from '../utils';
 
 const Hint = ({ showGMAL, worldPosition }) => {
   let hintContent = '';
+  let hintLabel = '';
   const platformPristine = worldPosition === FLAT;
+
 
   if (!showGMAL && platformPristine) {
     hintContent = 'find a lever';
+    hintLabel = (<b className="dark">hint&nbsp;&nbsp;</b>);
   } else if (showGMAL && platformPristine) {
     hintContent = 'find a place to stand';
+    hintLabel = (<b className="dark">hint&nbsp;&nbsp;</b>);
   } else {
     hintContent = 'move the world';
   };
 
   return (
     <p className="hint">
-      <b className="dark">hint&nbsp;&nbsp;</b>
+      {hintLabel}
       {hintContent}
     </p>
   );
