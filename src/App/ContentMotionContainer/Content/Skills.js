0b6bc45c092fbaa710fa07baee66cbfb3f3c52b2
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-09-03 17:44:42 -0400

    tweak spacing

--- src/App/ContentMotionContainer/Content/Skills.js
+++ src/App/ContentMotionContainer/Content/Skills.js
@@ -8,15 +8,15 @@ const Skills = () => (
         Veekas builds full-stack JavaScript applications.
       </p>
       <p className="side-view-paragraph no-margin-line">
-        <span className="font-weight-semibold">Languages:</span>
+        <span className="font-weight-semibold">Languages: </span>
         JavaScript, ES6+, HTML, CSS, Ruby, Python, SQL, R
       </p>
       <p className="side-view-paragraph no-margin-line">
-        <span className="font-weight-semibold">Libraries/Frameworks:</span>
+        <span className="font-weight-semibold">Libraries/Frameworks: </span>
         React, Redux, Webpack, Node.js, Axios, Express, Sequelize, Typescript, Jest, Enzyme, GraphQL, Apollo, D3.js, Mocha, Jasmine, AJAX, Socket.io, Bootstrap, Ruby on Rails, Styled-Components, Babel
       </p>
       <p className="side-view-paragraph no-margin-line">
-        <span className="font-weight-semibold">Tools:</span>
+        <span className="font-weight-semibold">Tools: </span>
         PostgreSQL, Git, Github, Codecov, Travis-CI, Chrome DevTools, JIRA, Microsoft Excel, VBA, Adobe Illustrator, Adobe Photoshop
       </p>
     </div>
