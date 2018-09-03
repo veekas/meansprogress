e958419afdfbb264ac6ce826fdf1ef24e1c34677
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-09-03 17:35:53 -0400

    reduce vertical height of quote if wide viewport

--- src/App/ContentMotionContainer/GiveMeALever/Quote/index.js
+++ src/App/ContentMotionContainer/GiveMeALever/Quote/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import './styles.css';
 
@@ -7,14 +7,22 @@ const GMAL_QUOTE_LINK = 'https://en.wikipedia.org/wiki/Archimedes';
 const Quote = ({ showGMAL }) => {
   const showOrHide = showGMAL ? 'quote-hide' : 'quote-show';
 
+  const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  const quoteText = (
+    width > height
+    ? "Give me a lever and a place to stand, and I shall move the world."
+    : (<Fragment>
+        Give me a lever and a place to stand,<br />
+        and I shall move the world."<br />
+      </Fragment>)
+  );
+
   return (
     <div className={`quote ${showOrHide} font-weight-light`}>
       <p className="quote-text">
-        <i>
-          "Give me a lever<br />
-          and a place to stand,<br />
-          and I shall move the world."<br />
-        </i>
+        <i>{quoteText}</i>
       </p>
       <a className="source" href={GMAL_QUOTE_LINK}>
         Archimedes
